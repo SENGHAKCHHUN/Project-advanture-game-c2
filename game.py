ae8f214275764f9908b0472db2fbbe015a7d6333
SENGHAK.CHHUN <SENGHAK.CHHUN@PNC.LAN>
2023-10-22 11:17:22 +0700

    #3 create home screen game

--- game.py
+++ game.py
@@ -12,3 +12,125 @@ GRAVITY_FORCE = 9
 JUMP_FORCE = 30
 SPEED = 7
 TIMED_LOOP = 6
+
+#============================ VARIABLES ============================
+
+keyPressed = []
+
+#============================ GLOBAL ============================
+score=0
+
+
+#============================ MAIN WINDOW ============================
+window = tk.Tk()
+window.geometry(str(WINDOW_WIDTH)+"x"+str(WINDOW_HEIGHT))
+window.title("ADVANTURE GAME")
+frame = tk.Frame()
+canvas = tk.Canvas(frame)
+
+#============================ IMAGES ============================
+start_img = PhotoImage(file="Images/start-button.png")
+help_btn = PhotoImage(file="Images/help-button.png")
+exit_img = PhotoImage(file="Images/exit-button.png")
+home_bg = PhotoImage(file='Images/bg-defualt.png')
+level_img = PhotoImage(file="Images/level.png")
+winter_bg = PhotoImage(file="Images/summer1.png")
+summer_bg = PhotoImage(file="Images/summer_bg.png")
+back_img = PhotoImage(file="Images/back.png")
+help_board = PhotoImage(file="Images/help.png")
+grass_img = PhotoImage(file="Images/grass.png",)
+stone_img = PhotoImage(file="Images/stone.png")
+coin_img = PhotoImage(file="Images/coin.png")
+door_img = PhotoImage(file="Images/door.png")
+key_img = PhotoImage(file="Images/key.png")
+flower_img = PhotoImage(file="Images/flower.png")
+money_img = PhotoImage(file="Images/money.png")
+thorn_img = PhotoImage(file="Images/thorn.png")
+dimond_img = PhotoImage(file="Images/dimond.png")
+monster_img = PhotoImage(file="Images/monster.png")
+level3_bg = PhotoImage(file="Images/level3_bg.png")
+player_img = PhotoImage(file="Images/player.png")
+
+
+
+
+
+# ======================= HOME_PAGE =============================
+def home():
+    canvas.create_image(0,0, image=home_bg, anchor="nw")
+    canvas.create_image(630, 300, image=start_img, anchor="nw", tags="start")
+    canvas.create_image(630, 370, image=help_btn, anchor="nw", tags="help")
+    canvas.create_image(630, 440, image=exit_img, anchor="nw", tags="exit")
+
+
+# ======================= BACK TO LEVELS PAGE =============================
+
+
+#============================ EXIT ============================
+def exit(event):
+    window.destroy()
+
+#=========================== FUNCTIONS MOVE PLAYER =======================
+def check_movement(dx=0, dy=0, checkGround=False):
+    coord = canvas.coords(player)
+    platforms = canvas.find_withtag("platform")
+    if coord[0] + dx < 0 or coord[1] + dy > WINDOW_WIDTH:
+        return False
+    if checkGround:
+        overlap = canvas.find_overlapping(coord[0], coord[1], coord[0]+ player_img.width(), coord[1] + player_img.height())
+    else:
+        overlap = canvas.find_overlapping(coord[0], coord[1], coord[0]+ player_img.width(), coord[1] + player_img.height())
+    print(overlap)
+    for platform in platforms:
+        if platform in overlap:
+            return False
+    return True
+def jump(force):
+    global player
+    if force > 0:
+        if check_movement(0, -force): 
+            canvas.move(player, 0, -force)
+    window.after(TIMED_LOOP, jump, force-1)
+def start_move(event):
+    if event.keysym not in keyPressed:
+        keyPressed.append(event.keysym)
+        print(keyPressed)
+        if len(keyPressed) == 1:
+            move()
+def move():
+    global player
+    if not keyPressed == []:
+        x = 0
+        if "Left" in keyPressed:
+            x -= SPEED
+        if "Right" in keyPressed:
+            x += SPEED
+        if "space" in keyPressed and not check_movement(0, GRAVITY_FORCE, True):   
+            jump(JUMP_FORCE)
+        if not check_movement(x):
+            canvas.move(player, x, 0)
+            window.after(TIMED_LOOP, move)
+def gravity():
+    if check_movement(0, GRAVITY_FORCE, True):
+        canvas.move(player, 0, GRAVITY_FORCE)
+        window.after(TIMED_LOOP, gravity)
+def stop_move(event):
+    global keyPressed
+    if event.keysym in keyPressed:
+        keyPressed.remove(event.keysym)
+
+
+#============================ KEY EVENT ============================
+
+
+#========================= REMOTES =================
+window.bind("<Key>", start_move)
+window.bind("<KeyRelease>", stop_move)
+
+
+home()
+
+#========================= DISPLAY WINDOW =================
+canvas.pack(expand=True, fill="both")
+frame.pack(expand=True, fill="both")
+window.mainloop()
